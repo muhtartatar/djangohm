bc97e57e34393c7fa99fde01a0eca3ae286ee7fd
muhtartatar <132200636+muhtartatar@users.noreply.github.com>
2023-05-15 18:03:51 +0100

    v_1

--- school/school/urls.py
+++ school/school/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from online_school import views
+from django.contrib import admin
+
 
 urlpatterns = [
     path('', views.add_user, name='add_user'),
@@ -7,4 +9,5 @@ urlpatterns = [
     path('choose_course/', views.choose_course, name='choose_course'),
     path('add_grade/', views.add_grade, name='add_grade'),
     path('students_list/', views.students_list, name='students_list'),
+    path('admin/', admin.site.urls),
 ]
